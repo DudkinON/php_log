17272466083e880ad9e3bb40f40f69d68736c158
Oleg Dudkin <dudkin.on@gmail.com>
2017-10-24 11:57:27 -0500

    Work version 0.1

---
+++ README.md
@@ -0,0 +1,54 @@
+# HTTP simple statistic
+
+This script provides fast processing Apache HTTP combined log with saving to MySQL database. 
+
+And display: 
+
+1. Amount of successful connections, amount errors connections, and their totals. 
+2. The top ten most popular links with their percents. 
+3. The top ten most popular user agents with their percents.
+
+## Installation
+
+Clone this script with GIT:
+
+```
+git clone https://github.com/DudkinON/php_log
+```
+
+in your project folder edit Apache config file **httpd.conf** or
+ **httpd-vhosts.conf** (if you use virtual hosts) add following code in 
+ **VirtualHost**:
+```
+ LogFormat "%h %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-agent}i\"" combined
+ CustomLog "/path/to/php_log/access_log" combined
+```
+> Note: edit **$LOG_FILE** if you changed name of Apache log file to something different from
+**access_log**.
+
+> Note: If you already have Apache log file with same **combined** **LogFormat**
+You can also copy the log to script folder and rename same name with **$LOG_FILE**
+
+> Note: After processing, the log file **will be rewrite** with null value. If you want, 
+you can save copy of log file into different folder.
+
+## Usage
+
+In the script file, change the following variables: $host, $db_name, $user, $password with your
+database connection data.
+
+For using this script, provide access to folder php log. This script have to be 
+available on url: www.example.com/php_log if you did not change the folder name. Click on 
+update log link, after that you will see the views data
+
+> Note: if you do not see the reports after clicking on the update log link, change
+ in the script file the following line:
+
+```
+ini_set('display_errors', 0);
+```
+on 
+```$xslt
+ini_set('display_errors', 1);
+```
+for display errors.
--- index.php
+++ index.php
@@ -8,34 +8,203 @@
  */
 
 
-// simple saving data
+define('MAX_ROWS', 100);
 
-function getConnection()
+// display errors
+ini_set('display_errors', 0);
+error_reporting(E_ALL);
+
+
+// Define time zone
+date_default_timezone_set('UTC');
+$LOG_FILE = 'access_log';
+$ERRORS = false; // Errors list
+
+
+function get_connection()
 {
+    /**
+     * TODO: Access to mysql database. Return PDO object or false
+     */
     $host = '127.0.0.1';
     $db_name = 'access_log';
     $user = 'log_app';
     $password = 'Strong_password';
+    $charset = 'utf8';
 
-    $db = new PDO("mysql:host=$host;dbname=$db_name", $user, $password);
-    $db->exec("set names utf8");
-    return $db;
+    $dsn = "mysql:host=$host;dbname=$db_name;charset=$charset";
+    $opt = [
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES => false,
+        PDO::ATTR_PERSISTENT => true,
+    ];
+    try {
+        $db = new PDO($dsn, $user, $password, $opt);
+        $db->exec("set names utf8");
+        return $db;
+    } catch (Exception $e) {
+        $ERRORS[] = 'Bad connection';
+        return false;
+    }
 }
-function save_connection()
+
+
+function save_log($data)
 {
-    $db = getConnection();
-    $sql = 'INSERT ' .
-        'INTO `log` (`id`, `uri`, `method`, `user_agent`, `status`, `date`, `ip`)' .
-        ' VALUES (NULL, :uri, :method, :user_agent, :status, :date, :ip)';
-
-    $data = $_SERVER;
-    $date = date('Y-m-d H:i:s');
-    $result = $db->prepare($sql);
-    $result->bindParam(':uri', $data['REQUEST_URI'], PDO::PARAM_STR);
-    $result->bindParam(':method', $data['REQUEST_METHOD'], PDO::PARAM_STR);
-    $result->bindParam(':user_agent', $data['HTTP_USER_AGENT'], PDO::PARAM_STR);
-    $result->bindParam(':status', $data['REDIRECT_STATUS'], PDO::PARAM_INT);
-    $result->bindParam(':date', $date, PDO::PARAM_STR);
-    $result->bindParam(':ip', $data['REMOTE_ADDR'], PDO::PARAM_STR);
+    /**
+     *  TODO: Saving log into database
+     */
+    $db = get_connection();
+    try {
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $db->beginTransaction();
+        foreach ($data as $elem) {
+            $uri = $elem['uri'];
+            $method = $elem['method'];
+            $date = $elem['date'];
+            $user_agent = $elem['user_agent'];
+            $status = $elem['status'];
+            $ip = $elem['ip'];
+            $db->exec("INSERT INTO `log` (`id`, `uri`, `method`, `user_agent`, `status`, `date`, `ip`) 
+                                 VALUES (NULL, '{$uri}', '{$method}', '{$user_agent}', '{$status}', '{$date}', '{$ip}')");
+        }
+        $db->commit();
+        return true;
+    } catch (Exception $e) {
+        $ERRORS[] = $e;
+        $db->rollBack();
+        return false;
+    }
 }
-save_connection();
+
+function convert_log_time($s)
+{
+    /**
+     * TODO: convert the date format from Apache to MySQL
+     */
+    $s = preg_replace('#:#', ' ', $s, 1);
+    $s = str_replace('/', ' ', $s);
+    if (!$t = strtotime($s)) return FALSE;
+    return date('Y-m-d', $t);
+}
+
+function get_log() {
+    /**
+     * TODO: Read data from Apache v2.4 log file
+     */
+    global $LOG_FILE, $ERRORS;
+    $log = [];
+    $handle = @fopen($LOG_FILE, "r");
+    if ($handle) {
+        while (($buffer = fgets($handle, 4096)) !== false) {
+            preg_match('~(\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3})\s.+?\s.+?\s\[(\d{1,2}\/\w{3}\/\d{4})[\d:]+\s[\d-]+\]\s\"([A-Z]+)\s([\S]+)\s(HTTP\/[\d\.]+)\"\s([\d]{3})\s[\S]+\s["-\s]{0,5}(.+)~', $buffer, $matches);
+            $date = convert_log_time($matches[2]);
+            if (isset($matches[0]) && $date) {
+                $log[] = [
+                    'ip' => $matches[1],
+                    'date' => $date,
+                    'method' => $matches[3],
+                    'uri' => $matches[4],
+                    'status' => $matches[6],
+                    'user_agent' => $matches[7]];
+            }
+            if (count($log) >= MAX_ROWS) {
+                save_log($log);
+                $log = [];
+            }
+        }
+
+        // Tests for end-of-file on a file pointer
+        if (!feof($handle)) $ERRORS[] = "Error: unexpected fgets() fail\n";
+
+        // Close an open file pointer
+        fclose($handle);
+
+        // if errors is none and save_log return true, clearing a log file
+        if (save_log($log) && $ERRORS === false) file_put_contents($LOG_FILE, '');
+        else echo '<pre>'; var_dump($ERRORS); echo '</pre>';
+    }
+}
+
+function get_access_by_status_code() {
+    /**
+     * TODO: Return the amount of connections by status code
+     */
+    $db=get_connection();
+
+    $result = $db->query('SELECT `status`, COUNT(*) AS `amount` FROM `log` GROUP BY `status`');
+
+    return $result->fetchAll();
+}
+
+function get_access_by_uri() {
+    /**
+     * TODO: Return amount of the most popular uri
+     */
+    $db=get_connection();
+
+    $result = $db->query('SELECT `uri`, ' .
+        'COUNT(uri) / (SELECT COUNT(*) FROM `log`) * 100 AS `percent` ' .
+        'FROM `log` ' .
+        'GROUP BY `uri` ' .
+        'ORDER BY `percent` DESC ' .
+        'LIMIT 10');
+
+    return $result->fetchAll();
+}
+
+function get_access_by_user_agent() {
+    /**
+     * TODO: Return amount of the most popular users agents
+     */
+    $db=get_connection();
+
+    $result = $db->query('SELECT `user_agent`, ' .
+        'COUNT(user_agent) / (SELECT COUNT(*) FROM `log`) * 100 AS `percent`' .
+        'FROM `log` ' .
+        'GROUP BY `user_agent`' .
+        'ORDER BY `percent` DESC ' .
+        'LIMIT 10');
+
+    return $result->fetchAll();
+}
+
+if (isset($_GET['log']) && $_GET['log'] === 'update') get_log();
+
+$access = get_access_by_status_code();
+$total = 0;
+$successes_access = 0;
+$errors_access = 0;
+echo '<pre>';
+echo '<h3>Status Code Connections</h3>';
+
+foreach ($access as $item) {
+    if ($item['status'] === 200) $successes_access += $item['amount'];
+    else $errors_access += $item['amount'];
+    echo "<b>status code:</b> {$item['status']} - amount: {$item['amount']}\n";
+    $total += $item['amount'];
+}
+
+echo "<br><b>Total:</b> {$total} <br>";
+echo "<b>Successes:</b>  {$successes_access} <br>";
+echo "<b>Errors:</b>  {$errors_access} <hr><br>";
+
+$access = get_access_by_uri();
+
+echo '<h3>URI connections</h3>';
+
+foreach ($access as $item) {
+    echo "<b>uri:</b> {$item['uri']} - {$item['percent']}%\n";
+}
+
+echo "<br><hr><br>";
+
+$access = get_access_by_user_agent();
+
+echo '<h3>User Agent Percents</h3>';
+
+foreach ($access as $item) {
+    echo "<b>user agent:</b> {$item['user_agent']} - {$item['percent']}%<br>";
+}
+
+echo "<br><hr><br><a href='/log.php?log=update'>update log</a>";
